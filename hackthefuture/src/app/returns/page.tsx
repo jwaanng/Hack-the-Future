4c2822fc7eedd9c1aa1056c68044165a14cd2f2d
jessie wang <117032848+jwaanng@users.noreply.github.com>
2025-03-07 02:00:48 -0500

    fix the returns page sizing and figure out how to add stuff logging off gn so far tho

--- hackthefuture/src/app/returns/page.tsx
+++ hackthefuture/src/app/returns/page.tsx
@@ -1,13 +1,23 @@
 import { ST } from "next/dist/shared/lib/utils";
 import Step from "./step";
+import CircularProgress from '@mui/joy/CircularProgress';
 
 export default function Returns() {
   return (
-    <div className="bg-[#e9fcff] text-coolGray-800 flex flex-col items-center justify-center h-screen space-y-4">
-      <Step stepNumber={1} description="Upload Video" routeTo="/returns/videoUpload" />
-      <Step stepNumber={2} description="Review and Confirm" routeTo="/returns/review" />
-      <Step stepNumber={3} description="Pack and Ship" routeTo="/returns/packAndShip" />
-      <Step stepNumber={4} description="Complete" routeTo="/returns/complete" />
+    <div>
+      <div className="bg-[#e9fcff] flex flex-row justify-between border border-red-500">
+        <div className="justify-items-start border border-red-500">
+          <div className="text-coolGray-800 flex flex-col items-center justify-center h-screen space-y-4 ml-35">
+            <Step stepNumber={1} description="Upload Video" routeTo="/returns/videoUpload" />
+            <Step stepNumber={2} description="Review and Confirm" routeTo="/returns/review" />
+            <Step stepNumber={3} description="Pack and Ship" routeTo="/returns/packAndShip" />
+            <Step stepNumber={4} description="Complete" routeTo="/returns/complete" />
+          </div>
+        </div>
+        <div className="justify-center w-full h-full p-1/2 border border-blue-500">
+          <CircularProgress determinate value={75} size="lg" className="w-full"/>
+        </div>
+      </div>
     </div>
   );
 }
--- hackthefuture/src/app/returns/step.tsx
+++ hackthefuture/src/app/returns/step.tsx
@@ -9,7 +9,7 @@ export default function Step({ stepNumber, description, routeTo }: { stepNumber:
   };
 
   return (
-    <div className="bg-black rounded-lg p-6 m-4 cursor-pointer w-72" onClick={handleClick}>
+    <div className="bg-black rounded-lg p-6 m-4 cursor-pointer w-96" onClick={handleClick}>
       <h2 className="text-2xl text-white text-base">{stepNumber}. {description}</h2>
     </div>
   );
